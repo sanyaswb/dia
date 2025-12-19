d6639d3f9f3f4543ffc664e154b3a3073fd66d1c
Oleksandr Merkulov <odin@terradeus.org>
2025-12-19 14:05:23 +0200

    optimizated images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,7 @@
-'use strict';
+import "./blocks/smart-loader.js";
+import "./blocks/division.js";
+import "./blocks/menu.js";
+import "./blocks/nav-bar.js";
+import "./blocks/parallax-scroll.js";
+import "./blocks/slider.js";
+import "./blocks/theme.js";
